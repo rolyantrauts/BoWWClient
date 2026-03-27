86f5f899584533e2a8bbc5f32d53652c9404c83b
rolyantrauts <rolyantrauts@gmail.com>
2026-03-27 10:43:17 +0000

    Add files via upload

--- include/WebSocketClient.h
+++ include/WebSocketClient.h
@@ -1,53 +1,36 @@
 #pragma once
-
-#define ASIO_STANDALONE
-#define _WEBSOCKETPP_CPP11_STL_
-
-#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
-#include <nlohmann/json.hpp>
-
+#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <string>
 #include <vector>
-#include <thread>
-#include <atomic>
 #include <functional>
+#include <nlohmann/json.hpp>
 
-typedef websocketpp::client<websocketpp::config::asio_client> ws_client;
+typedef websocketpp::client<websocketpp::config::asio_client> client;
 
 class WebSocketClient {
 public:
     WebSocketClient();
-    ~WebSocketClient();
-
     bool connect(const std::string& uri);
-    void disconnect();
+    void close();
 
     void send_hello(const std::string& guid);
+    void send_enroll(); 
     void send_confidence(float score);
     void send_audio(const std::vector<int16_t>& pcm_data);
-
+    
+    // Callbacks
     std::function<void()> on_connected;
     std::function<void()> on_disconnected;
+    std::function<void(std::string)> on_guid_assigned;
+    std::function<void(std::string)> on_temp_id_assigned; 
     std::function<void()> on_start_command;
     std::function<void()> on_stop_command;
-    std::function<void(std::string)> on_guid_assigned;
 
 private:
-    ws_client endpoint_;
-    websocketpp::connection_hdl hdl_;
-    
-    std::thread asio_thread_;
-    std::thread ping_thread_;
-    
-    std::atomic<bool> connected_{false};
-    std::atomic<bool> connection_failed_{false};
-    std::atomic<bool> running_{true};
+    client c;
+    websocketpp::connection_hdl hdl;
+    bool connected = false;
 
-    void on_open(websocketpp::connection_hdl hdl);
-    void on_close(websocketpp::connection_hdl hdl);
-    void on_fail(websocketpp::connection_hdl hdl);
-    void on_message(websocketpp::connection_hdl hdl, ws_client::message_ptr msg);
-    
-    void ping_loop();
+    void send_text(const std::string& msg);
 };
