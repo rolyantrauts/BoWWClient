3bb0b482d79ea33d04d26f6119dd758d3e65b4d2
rolyantrauts <rolyantrauts@gmail.com>
2026-03-28 09:07:19 +0000

    fix alsa methods and file queueing

--- include/AudioRingBuffer.h
+++ include/AudioRingBuffer.h
@@ -1,43 +1,50 @@
 #pragma once
-
 #include <vector>
 #include <mutex>
 #include <cstdint>
-#include <algorithm>
 
 class AudioRingBuffer {
 public:
-    AudioRingBuffer(size_t max_samples) 
-        : buffer_(max_samples), max_size_(max_samples), head_(0), count_(0) {}
+    AudioRingBuffer(size_t capacity) : capacity_(capacity), head_(0), size_(0) {
+        buffer_.resize(capacity, 0);
+    }
 
     void push(const std::vector<int16_t>& data) {
-        std::lock_guard<std::mutex> lock(mtx_);
+        std::lock_guard<std::mutex> lock(mutex_);
         for (int16_t sample : data) {
             buffer_[head_] = sample;
-            head_ = (head_ + 1) % max_size_;
-            if (count_ < max_size_) count_++;
+            head_ = (head_ + 1) % capacity_;
+            if (size_ < capacity_) size_++;
         }
     }
 
     std::vector<int16_t> flush() {
-        std::lock_guard<std::mutex> lock(mtx_);
+        std::lock_guard<std::mutex> lock(mutex_);
         std::vector<int16_t> out;
-        out.reserve(count_);
+        if (size_ == 0) return out;
         
-        size_t tail = (head_ + max_size_ - count_) % max_size_;
-        for (size_t i = 0; i < count_; ++i) {
-            out.push_back(buffer_[tail]);
-            tail = (tail + 1) % max_size_;
+        out.reserve(size_);
+        size_t start = (size_ < capacity_) ? 0 : head_;
+        for (size_t i = 0; i < size_; ++i) {
+            out.push_back(buffer_[(start + i) % capacity_]);
         }
-        
-        count_ = 0; 
         return out;
+    }
+
+    // --- NEW: Safely resize and clear the buffer dynamically ---
+    void resize(size_t new_capacity) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        capacity_ = new_capacity;
+        buffer_.clear();
+        buffer_.resize(capacity_, 0);
+        head_ = 0;
+        size_ = 0;
     }
 
 private:
     std::vector<int16_t> buffer_;
-    size_t max_size_;
+    size_t capacity_;
     size_t head_;
-    size_t count_;
-    std::mutex mtx_;
+    size_t size_;
+    std::mutex mutex_;
 };
--- include/WebSocketClient.h
+++ include/WebSocketClient.h
@@ -1,36 +1,47 @@
 #pragma once
-#include <websocketpp/client.hpp>
-#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <string>
 #include <vector>
 #include <functional>
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
 #include <nlohmann/json.hpp>
+#include <thread>
+#include <mutex>
 
-typedef websocketpp::client<websocketpp::config::asio_client> client;
+using WsClient = websocketpp::client<websocketpp::config::asio_client>;
 
 class WebSocketClient {
 public:
     WebSocketClient();
+    ~WebSocketClient();
+
     bool connect(const std::string& uri);
-    void close();
+    void disconnect();
 
     void send_hello(const std::string& guid);
-    void send_enroll(); 
+    void send_enroll();
     void send_confidence(float score);
     void send_audio(const std::vector<int16_t>& pcm_data);
-    
-    // Callbacks
+
     std::function<void()> on_connected;
     std::function<void()> on_disconnected;
+    std::function<void(std::string)> on_temp_id_assigned;
     std::function<void(std::string)> on_guid_assigned;
-    std::function<void(std::string)> on_temp_id_assigned; 
     std::function<void()> on_start_command;
     std::function<void()> on_stop_command;
+    
+    // --- NEW: Handshake Callback ---
+    std::function<void(float)> on_hello_ack; 
 
 private:
-    client c;
-    websocketpp::connection_hdl hdl;
-    bool connected = false;
+    WsClient client_;
+    websocketpp::connection_hdl hdl_;
+    std::thread ws_thread_;
+    bool is_connected_ = false;
+    std::mutex mtx_;
 
-    void send_text(const std::string& msg);
+    void on_open(websocketpp::connection_hdl hdl);
+    void on_close(websocketpp::connection_hdl hdl);
+    void on_message(websocketpp::connection_hdl hdl, WsClient::message_ptr msg);
+    void send_json(const nlohmann::json& j);
 };
